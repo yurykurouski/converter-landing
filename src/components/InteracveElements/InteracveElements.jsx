b51c49835fce8c5fe3e8f5e1fcf63d002cc75d38
Yury Kurouski <yury_kurouski@epam.com>
2023-12-15 19:33:19 +0100

    feat: update links

--- src/components/InteracveElements/InteracveElements.jsx
+++ src/components/InteracveElements/InteracveElements.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useHandleScroll } from './InteracveElements.hooks';
 
-import TelegramIcon from '../../assets/images/telegram-badge.png';
+// import TelegramIcon from '../../assets/images/telegram-badge.png';
+import GitHubIcon from '../../assets/images/github-mark.png';
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 export const InteracveElements = React.memo(({ onScrollDown }) => {
   const [handleTouchStart, handleTouchEnd, isDownScroll] = useHandleScroll(onScrollDown)
@@ -14,9 +16,14 @@ export const InteracveElements = React.memo(({ onScrollDown }) => {
       onTouchEnd={handleTouchEnd}
     >
       <section className={`elements-container ${ isDownScroll ? 'hidden' : 'visible' }`}>
-        <a href="https://t.me/fancyconverter" target="_blank" rel="noreferrer">
+        {/* <a href="https://t.me/fancyconverter" target="_blank" rel="noreferrer">
           <img src={TelegramIcon} className='elements_icon' />
+        </a> */}
+        <a href="https://github.com/yurykurouski/Fancy-converter" target="_blank" rel="noreferrer">
+          <img src={GitHubIcon} className='elements_icon' />
         </a>
+
+        <Link to='privacy' className='privacy-link'>Privacy</Link>
       </section>
     </div>
   )
